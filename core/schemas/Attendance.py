a6b8b3e4b457db00949f0e8b4ff479e5e4792f61
Enrique Ajin <enriquesaon@gmail.com>
2024-05-14 22:23:43 +0200

    Delete field id in Attendance model in order to remove ID duplicity. Remove response_model attribute for all of the endpoints.

--- core/schemas/Attendance.py
+++ core/schemas/Attendance.py
@@ -1,6 +1,7 @@
 def individual_serial(attendance) -> dict:
     return {
         "id": str(attendance["_id"]),
+        "is_attending": bool(attendance["is_attending"]),
         "name": attendance["name"],
         "attendees_quantity": int(attendance["attendees_quantity"]),
         "message": attendance["message"]
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from v1.routers import attendees
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Routers
 app.include_router(attendees.router)
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
